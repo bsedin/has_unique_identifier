d36eefe22a96f59e902c55b2bca8056bd97bb175
Sergey Besedin <kr3ssh@gmail.com>
2016-03-21 20:13:30 +0300

    Inherit has_unique_identifier from superclass

--- lib/has_unique_identifier/class_methods.rb
+++ lib/has_unique_identifier/class_methods.rb
@@ -1,7 +1,10 @@
 module HasUniqueIdentifier
   module ClassMethods
     def unique_identifier
-      @unique_identifier
+      return @unique_identifier if @unique_identifier
+      if superclass.respond_to?(:unique_identifier)
+        return superclass.unique_identifier
+      end
     end
 
     def has_unique_identifier(name, opts = {})
